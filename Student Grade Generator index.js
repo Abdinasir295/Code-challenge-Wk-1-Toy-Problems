4226dcd63168d8214659b0d6014be80e998ffc9a
Abdinasir295 <abdinasirmehdi62@gmail.com>
2024-03-27 11:17:56 +0300

    updated

--- Student Grade Generator index.js
+++ Student Grade Generator index.js
@@ -1,26 +1,27 @@
-
-function studentGrade(mark) {
+// declared a function with parameter 'mark'
+function studentGrade(mark) {   
+    // this ia an if condition 
         if (mark > 79) {    
-            return A
+            return 'A'
         }   
+        // else if condition if the first condition failed
         else if (60<= mark <=79) {  
-            return B
+            return 'B'
         }   
         else if (59<= mark <= 49){  
-            return C
+            return 'C'
         }   
         else if (40 <= mark <=49) { 
-            return D
-        }   
+            return 'D'
+        }       
+        //otherwise if all the above conditions failed
         else {  
-            return E
+            return 'E'
         }
-    }
-    function calculateGrade() {
-        var mark = prompt("Enter your marks:");
-        var grade = studentGrade(parseint(mark));
-        alert("Your grade is: " + grade);
-    }
+    }   
+    // input your marks here to know your grade
+    console.log(studentGrade());
+
     
     
 
